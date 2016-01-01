027f9202b8732824d180499cc3005a83ec0519e3
HankChow <hankchow@hankchow.net>
2016-01-01 20:29:38 +0800

    add counter-quad-centrosymmetry and remove y-axial-symmetry

--- avatar_generator.py
+++ avatar_generator.py
@@ -27,16 +27,16 @@ class avatar_generator:
 		if eval('0x' + md5str[-20]) % 4 == 0:
 			symmetry_method = 'x_axial_symmetry'
 		elif eval('0x' + md5str[12]) % 4 == 1:
-			symmetry_method = 'y_axial_symmetry'
-		elif eval('0x' + md5str[12]) % 4 == 2:
 			symmetry_method = 'semi_centrosymmetry'
-		else:
+		elif eval('0x' + md5str[12]) % 4 == 2:
 			symmetry_method = 'quad_centrosymmetry'
+		else:
+			symmetry_method = 'counter_quad_centrosymmetry'
 		avatarDetails = []
 		md5_block = []
 		for i in range(0, 4):
 			md5_block.append(md5str[(i * 3):((i + 1) * 3)])
-		if symmetry_method == 'quad_centrosymmetry':
+		if symmetry_method == 'quad_centrosymmetry' or symmetry_method == 'counter_quad_centrosymmetry':
 			for i in range(0, 4):
 				avatarDetails.append(str(bin(eval('0x' + md5_block[i][1])))[2:].zfill(4))
 		else:
@@ -53,7 +53,7 @@ class avatar_generator:
 		
 	def meta_generator(self, avatarInfo, x_len = 400, y_len = 400, block_len = 50):
 		meta = ''
-		if avatarInfo['symmetry_method'] == 'quad_centrosymmetry':
+		if avatarInfo['symmetry_method'] == 'quad_centrosymmetry' or avatarInfo['symmetry_method'] == 'counter_quad_centrosymmetry':
 			meta = Image.new('RGB', (x_len / 2, y_len / 2))
 			blocks = ImageDraw.Draw(meta)
 			x_count = 0
@@ -62,22 +62,6 @@ class avatar_generator:
 				for y in range(0, y_len / 2, block_len):
 					blocks_poly = [(x, y), (x + block_len, y), (x + block_len, y + block_len), (x, y + block_len)]
 					if avatarInfo['avatarDetails'][x_count][y_count] == '0':
-						blocks.polygon(blocks_poly, fill = avatarInfo['background'])
-					else:
-						blocks.polygon(blocks_poly, fill = avatarInfo['foreground'])
-					y_count += 1
-				y_count = 0
-				x_count += 1
-			return {'meta' : meta, 'sym_info' : avatarInfo['symmetry_method']}
-		elif avatarInfo['symmetry_method'] == 'y_axial_symmetry':
-			meta = Image.new('RGB', (x_len / 2, y_len))
-			blocks = ImageDraw.Draw(meta)
-			x_count = 0
-			y_count = 0
-			for x in range(0, x_len / 2, block_len):
-				for y in range(0, y_len, block_len):
-					blocks_poly = [(x, y), (x + block_len, y), (x + block_len, y + block_len), (x, y + block_len)]
-					if avatarInfo['avatarDetails'][y_count][x_count] == '0':
 						blocks.polygon(blocks_poly, fill = avatarInfo['background'])
 					else:
 						blocks.polygon(blocks_poly, fill = avatarInfo['foreground'])
@@ -119,8 +103,8 @@ class avatar_generator:
 		avatar = ''
 		if meta['sym_info'] == 'x_axial_symmetry':
 			avatar = self.x_axial_symmetry(meta['meta'])
-		elif meta['sym_info'] == 'y_axial_symmetry':
-			avatar = self.y_axial_symmetry(meta['meta'])
+		elif meta['sym_info'] == 'counter_quad_centrosymmetry':
+			avatar = self.counter_quad_centrosymmetry(meta['meta'])
 		elif meta['sym_info'] == 'semi_centrosymmetry':
 			avatar = self.semi_centrosymmetry(meta['meta'])
 		else:
@@ -155,6 +139,14 @@ class avatar_generator:
 		avatar.paste(meta.rotate(270), (meta.size[0], 0))
 		return avatar
 	
+	def counter_quad_centrosymmetry(self, meta):
+		avatar = Image.new('RGB', (meta.size[0] * 2, meta.size[1] * 2))
+		avatar.paste(meta, (0, 0))
+		avatar.paste(meta.rotate(270), (0, meta.size[1]))
+		avatar.paste(meta.rotate(180), (meta.size[0], meta.size[1]))
+		avatar.paste(meta.rotate(90), (meta.size[0], 0))
+		return avatar
+		
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
 		sys.argv.append(str(time.time()))
